705260dc52e78e3e0fc288aa7e9ca69c6062a728
m2sar <m2sar@ppti-14-409-05.ufr-info-p6.jussieu.fr>
2019-05-10 11:43:52 +0200

    Save

--- SwiftProject/SwiftProject/Assets.xcassets/AppIcon.appiconset/DetailsFav.swift
+++ SwiftProject/SwiftProject/Assets.xcassets/AppIcon.appiconset/DetailsFav.swift
@@ -1,24 +1,21 @@
 //
-//  ViewController.swift
+//  DetailsFav.swift
 //  SwiftProject
 //
-//  Created by m2sar on 07/05/2019.
+//  Created by m2sar on 10/05/2019.
 //  Copyright © 2019 m2sar. All rights reserved.
 //
 
 import UIKit
 import Weather
 
-class ViewController: UIViewController {
+class DetailsFav: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let paris = WeatherClient(key: "9e6d39413722f1a451125d937bf8b5b9").citiesSuggestions(for: "Paris")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     
 }
-
