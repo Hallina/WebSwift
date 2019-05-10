c480258921e70539c35c10dcd8a69046c9df7302
m2sar <m2sar@ppti-14-409-05.ufr-info-p6.jussieu.fr>
2019-05-10 13:43:59 +0200

    Change image favori

--- SwiftProject/SwiftProject/Assets.xcassets/AppIcon.appiconset/DetailsFav.swift
+++ SwiftProject/SwiftProject/Assets.xcassets/AppIcon.appiconset/DetailsFav.swift
@@ -18,5 +18,24 @@ class DetailsFav: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func clickButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Confirm", message: "Are you sure you want to delete it from your favorites?", preferredStyle: .alert)
+        let clearAction = UIAlertAction(title: "Delete", style: .destructive) { (alert: UIAlertAction!) -> Void in
+            self.StarButton.isSelected = false
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { (alert: UIAlertAction!) -> Void in
+            print("You pressed Cancel")
+        }
+        
+        if StarButton.isSelected == true{
+            alert.addAction(clearAction)
+            alert.addAction(cancelAction)
+            
+            present(alert, animated: true, completion:nil)
+        }
+        else {
+            StarButton.isSelected = true
+        }
+    }
     
 }
