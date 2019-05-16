a056a7b44c90f362f27cd20029a02674059cb179
m2sar <m2sar@ppti-14-409-05.ufr-info-p6.jussieu.fr>
2019-05-16 16:17:59 +0200

    SearchBar

--- SwiftProject/SwiftProject/Recherche.swift
+++ SwiftProject/SwiftProject/Recherche.swift
@@ -14,15 +14,94 @@ class Recherche: UIViewController {
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var cityName: UITextField!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tbView: UITableView!
+    
+    var cities: [City]!
+    let weatherClient = WeatherClient(key: "9e6d39413722f1a451125d937bf8b5b9")
+    
+    var citiesName = [String]()
+    var citiesID = [Int64]()
+    var citiesCountry = [String]()
+    var searching = false
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchBar.delegate = self
+        //cities = WeatherClient(key: "9e6d39413722f1a451125d937bf8b5b9").citiesSuggestions(for:  searchBar ?? "")
     }
     
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         if let searchDetail = segue.destination as? DetailsRecherche {
             searchDetail.query = cityName.text
         }
+    }
+}
+
+extension Recherche: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return citiesName.count
+        }
+        else {
+            return 1
+        }
+    }
+    
+    func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "City")
+        if searching {
+            cell?.textLabel?.text = citiesName[indexPath.row] + " " + citiesCountry[indexPath.row]
+        }
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        index = indexPath.row
+        performSegue(withIdentifier: "segueWeatherCity", sender: self)
+    }
+}
+
+extension Recherche: UISearchBarDelegate {
+    func searchBarSelect(_ searchBar: UISearchBar) {
+        // clean datas
+        citiesName.removeAll()
+        citiesID.removeAll()
+        citiesCountry.removeAll()
+        
+        // search from the text entered
+        searchCities(searchText: searchBar.text!)
+        
+        // prepare for table view
+        for city in cities {
+            citiesName.append(city.name)
+            citiesID.append(city.identifier)
+            citiesCountry.append(city.country)
+        }
+        
+        searching = true
+        tbView.reloadData() // reload the tableview
+    }
+    
+    func searchCities (searchText: String) {
+        cities = nil
+        if (searchText != ""){
+            cities = weatherClient.citiesSuggestions(for: searchText)
+        }
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchBar.text = ""
+        tbView.reloadData()
+        searchBar.endEditing(true)
     }
 }
